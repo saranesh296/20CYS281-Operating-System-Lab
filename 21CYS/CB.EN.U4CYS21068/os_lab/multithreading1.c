f3221f5f2aeb7566159fac397812bc0f30d6a274
saranesh296 <55594085+saranesh296@users.noreply.github.com>
2022-12-22 11:26:43 +0530

    Add files via upload

---
+++ 21CYS/CB.EN.U4CYS21068/os_lab/multithreading1.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <unistd.h>
+
+// printWelcomeMessage will be called when the Thread is created in the main function 
+// which takes string as an argument
+void *printWelcomeMessage(void *threads) {
+    
+   sleep(2);    
+   long *tid=(long *)threads;
+   printf("\n[THREAD] Hello, Welcome %ld.",*tid);
+   pthread_exit(NULL);
+   
+}
+
+int main () {
+
+   // thread defintion
+   pthread_t threads[10];
+   
+   // parameter to be passed to the called function - printWelcomeMessage
+   char names[10][15] = {"Amritha","Praveen","Saurabh","Sangeetha","Lakshmy","Srinivasan","Ramaguru"};
+   
+   int result;
+   
+   for(int i = 0; i < 7; i++ ) {
+   
+      printf("\n[MAIN] Creating thread, %d", i);
+      
+      // Creating the threading and thus calling the function with parameter passed to it
+      result = pthread_create(&threads[i], NULL, printWelcomeMessage, (void *)&threads[i]);
+      
+      if (result) {
+      
+         printf("Error in creating thread, %d ", result);
+         exit(-1);
+      }
+      
+   }
+   
+   // Exit the thread
+   pthread_exit(NULL);
+}
